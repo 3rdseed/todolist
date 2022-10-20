59fff379d6d8f35a4cd5518ecea3c596777a73cf
jisic <113878434+jisic@users.noreply.github.com>
2022-10-20 14:45:36 +0900

    Update configStore.js

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import todos from "../modules/todosSlice";
+import commentsSlice from "../modules/commentsSlice";
 
 const store = configureStore({
-    reducer: { todos }
+    reducer: { todos, commentsSlice} 
 })
 
 export default store;
